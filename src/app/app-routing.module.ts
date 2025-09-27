8996592071234a624ba3043f70548468fd3a7c42
whatever <you@example.com>
2025-09-27 17:40:19 -0300

    Added missing files. Pages folder created. 4 Pages crafted and with navigation implemented (Home, Community, Register and Login)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,29 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'teacher-page',
+    loadChildren: () => import('./pages/teacher-page/teacher-page.module').then( m => m.TeacherPageModule)
+  },
+  {
+    path: 'community',
+    loadChildren: () => import('./pages/community/community.module').then( m => m.CommunityPageModule)
   },
 ];
 
